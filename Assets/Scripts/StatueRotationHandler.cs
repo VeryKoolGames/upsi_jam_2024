089bb14655a502d6f947e614fee19a5551546aff
Victor Vermot <victorvermot@gmail.com>
2024-03-23 14:04:00 +0100

    rotation statues

--- Assets/Scripts/StatueRotationHandler.cs
+++ Assets/Scripts/StatueRotationHandler.cs
@@ -6,32 +6,67 @@ using UnityEngine;
 
 public class StatueRotationHandler : MonoBehaviour
 {
-    [SerializeField] private Animator statueAnimator;
     [SerializeField] private VerifyStatues _verifyStatues;
     public Orientation currentOrientation;
     [SerializeField] private Orientation startOrientation;
+    [SerializeField] private AudioSource rotateSound;
+    private bool isRotating;
+    private float currentRotation;
+    private float targetRotation;
+    private float rotationSpeed = 40.0f;
 
     private void Start()
     {
         currentOrientation = startOrientation;
+        currentRotation = transform.rotation.y;
+    }
+
+    private void Update()
+    {
+        if (isRotating)
+        {
+            currentRotation += rotationSpeed * Time.deltaTime;
+        
+            if (currentRotation >= targetRotation)
+            {
+                currentRotation = targetRotation; // Corrects the overshoot to make sure it ends exactly at targetRotation
+                isRotating = false; // Stops the rotation
+                currentOrientation++;
+                if (currentOrientation > Orientation.SOUTH)
+                {
+                    currentOrientation = Orientation.EAST;
+                }
+                _verifyStatues.VerifyStatuesOrientation();
+                StartCoroutine(FadeOutCoroutine(1f));
+            }
+
+            transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+        }
     }
 
     private void OnMouseDown()
     {
-        StartCoroutine(rotateStatue());
-    }
-
-    IEnumerator rotateStatue()
-    {
-        statueAnimator.SetBool("rotateBool", value:true);
-        yield return new WaitForSeconds(1.5f);
-        statueAnimator.SetBool("rotateBool", value:false);
-        currentOrientation++;
-        if (currentOrientation > Orientation.SOUTH)
+        if (!isRotating)
         {
-            currentOrientation = Orientation.EAST;
+            targetRotation = currentRotation + 90;
+            rotateSound.Play();
         }
-        _verifyStatues.VerifyStatuesOrientation();
+        isRotating = true;
+    }
+    
+
+    private IEnumerator FadeOutCoroutine(float duration)
+    {
+        float startVolume = rotateSound.volume;
+
+        while (rotateSound.volume > 0)
+        {
+            rotateSound.volume -= startVolume * Time.deltaTime / duration;
+            yield return null;
+        }
+
+        rotateSound.Stop();
+        rotateSound.volume = startVolume; // Optionally reset the volume back to start volume if you plan to play it again
     }
     
 }
